a46e52f9391aca0369b4685dccf228de860f064d
anoopyadavan <anoopyadavan@gmail.com>
2020-05-26 02:32:27 +0530

    updating textureSms file

--- textureSms.py
+++ textureSms.py
@@ -3,15 +3,16 @@ import json
 import os
 from time import sleep
 import sys
+from termcolor import colored
 
 URL = 'https://www.sms4india.com/api/v1/sendCampaign'
 
 # titlebar
 def title_bar():
     os.system("clear")
-    print("\t\t\t ----------------------------------------------------------------")
-    print("\t\t\t|        W E L C O M E   T O   T E X T U R E    S M S           |")
-    print("\t\t\t ----------------------------------------------------------------\n")
+    print("\t\t\t ---------------------------------------------------")
+    print("\t\t\t|   W E L C O M E   T O   T E X T U R E    S M S    |")
+    print("\t\t\t ---------------------------------------------------\n")
 # get request
 def sendPostRequest(reqUrl, apiKey, secretKey, useType, phoneNo, senderId, textMessage):
   req_params = {
@@ -27,18 +28,18 @@ def sendPostRequest(reqUrl, apiKey, secretKey, useType, phoneNo, senderId, textM
 title_bar()
 try:
     while True:
-        num=input('Enter mobile number: ')
+        num=input('[+] Enter mobile number: ')
         if not num.isdigit():
-            print("Only numbers are accepted\n")
+            print(colored("[!!] Only numbers are accepted\n", 'red'))
             continue
         elif len(num)!=10:
-            print("10 Digit number is accepted\n")
+            print(colored("[!!] 10 Digit number is accepted\n", 'red'))
             continue
         else:
             break
-    msg=input('Enter message: ')
+    msg=input('[+] Enter message: ')
     # get response
-    response = sendPostRequest(URL, 'CYQCG29KDVD7BO6BMXA8ZTR5H405386G', 'FHYKK6I8D564QF29', 'stage', num , 'l3xi3', msg )
+    response = sendPostRequest(URL, '96RN5RK6G8NVG6Z47FRSNPVH6DGT86C2', 'ZZ8YUDJQAIZV6LBO', 'stage', num , 'l3xi3', msg )
     """
     Note:-
     you must provide apikey, secretkey, usetype, mobile, senderid and message values
